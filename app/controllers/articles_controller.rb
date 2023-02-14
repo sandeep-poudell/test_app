b6e9c046335c9a255ed7a5d4e56948bbda3ee7a0
Sandeep Poudel <poudel.sandeep@cloco.co.jp>
2023-02-14 11:13:55 +0900

    Added article CRUD functionalities to frontend, Bootstrap

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,10 +1,53 @@
 class ArticlesController < ApplicationController
 
+    before_action :set_article, only: [:show, :edit, :update, :destroy]
+    
     def index
-
+        @articles = Article.all
     end
 
     def show
+    end
+
+    def new
+        @article = Article.new
+    end
+
+    def create
+        @article = Article.new(params.require(:article).permit(:title, :description))
+        #byebug
+        if @article.valid?
+            @article.save
+            flash[:notice] = "Article was created successfully"
+            redirect_to @article
+        else
+            render :new, status: :unprocessable_entity
+        end
+    end
+
+    def edit
+    end
+
+    def update
+        @article = Article.find(params[:id])
+        if @article.update(params.require(:article).permit(:title, :description))
+            flash[:notice] = "Article was updated successfully"
+            redirect_to @article
+        else
+            render :edit, status: :unprocessable_entity
+        end
+    end
+
+    def destroy
+        if @article.destroy
+            flash[:notice] = "The article was deleted successfully"
+            redirect_to articles_path
+        end
+    end
+
+    private
+
+    def set_article
         @article = Article.find(params[:id])
     end
 
--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,5 @@ Rails.application.routes.draw do
   root 'pages#home'
   get 'about', to: 'pages#about'
   resources :articles
-
 end
-
 
