4767102cae50d9b94682fb62d6654175a3273d77
Sandeep Poudel <poudel.sandeep@cloco.co.jp>
2023-02-15 10:19:19 +0900

    Add user validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,6 @@
 class User < ApplicationRecord
+  validates :username, presence: true, uniqueness: { case_sensitive: false }, length: {:minimum => 3, :maximum => 25}
+  validates :email, presence: true, uniqueness: { case_sensitive: false }, length: {:maximum => 105}, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i }
 
+  has_many :articles
 end
